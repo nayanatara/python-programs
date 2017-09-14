9c427c4447ef7d486c44f2ceaaa9b73537108146
nayanatara <31612425+nayanatara@users.noreply.github.com>
2017-09-14 09:57:50 +0000

    Add files via upload

---
+++ area_of_a_circle.py
@@ -0,0 +1,12 @@
+Python 3.6.2 (default, Jul 20 2017, 08:43:29) 
+[GCC 6.3.0 20170406] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> r=3
+>>> area=3.14*r*r
+>>> print(area)
+28.259999999999998
+>>> r=567
+>>> area=3.14*r*r
+>>> print(area)
+1009475.4600000001
+>>> quit()
