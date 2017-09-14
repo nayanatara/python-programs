02de8b3bb93fc372b397471380f2561d9af9bbd6
nayanatara <31612425+nayanatara@users.noreply.github.com>
2017-09-14 09:43:10 +0000

    Delete text7.py

--- text7.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import smtplib
-s=smtplib.SMTP('smtp.gmail.com',587)
-s.starttls()
-s.login("nayanaputtadev@gmail.com","9945311055")
-message="hello"
-s.sendmail("nayanaputtadev@gmail.com","nayanaputtadev@gmail.com",message)
-print("sucess")
-s.quit()
